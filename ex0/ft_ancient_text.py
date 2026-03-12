1564d5a893eceef813c60dcfdb5f830b08fcda3a
lmp2000 <lmrpais@gmail.com>
2026-03-12 00:48:11 +0000

    commit

---
+++ ex0/ft_ancient_text.py
@@ -0,0 +1,26 @@
+def main() -> None:
+    print(
+        '=== CYBER ARCHIVES - DATA RECOVERY SYSTEM ===\n'
+        )
+
+    print(f'Accessing Storage Vault: ancient_fragment.txt')
+    try:
+        f = open("ancient_fragment.txt", "r")
+        print('Connection established\n')
+    except FileNotFoundError:
+        print(
+            'ERROR: Storage vault not found. Run data generator first'
+              )
+        return
+
+    content = f.read()
+    print('RECOVERED DATA:')
+    print(content)
+
+    f.close()
+
+    print('\nData recovery complete. Storage unit disconnected.')
+
+
+if __name__ == "__main__":
+    main()
